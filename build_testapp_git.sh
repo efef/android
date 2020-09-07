77f18b74392dd4c618ed94cf7935d1480a5a9599
rogier <github@spoor.nu>
2020-09-07 16:26:49 +0200

    Update build_testapp_git.sh
    newest FrKo version

--- build_testapp_git.sh
+++ build_testapp_git.sh
@@ -8,20 +8,20 @@ SDK_DIR=${HOME}/android-sdk
 KEY_STORE=${HOME}/android.jks
 
 GIT_REPO=https://github.com/eduvpn/android
-GIT_TAG=2.0.0
-#GIT_TAG=master
+#GIT_TAG=2.0.0
+GIT_TAG=master
 
 PROJECT_DIR=${HOME}/Projects
 APP_DIR=${PROJECT_DIR}/eduvpn-android-$(date +%Y%m%d%H%M%S)
 
 # eduVPN
-GRADLE_TASK=app:assembleBasicRelease
-UNSIGNED_APK=${APP_DIR}/app/build/outputs/apk/basic/release/app-basic-release-unsigned.apk 
+GRADLE_TASK=app:assembleBasicDebug
+UNSIGNED_APK=${APP_DIR}/app/build/outputs/apk/basic/debug/app-basic-debug.apk
 SIGNED_APK=${PROJECT_DIR}/eduVPN-${GIT_TAG}.apk
 
 # eduVPN Test
-GRADLE_TEST_TASK=app:assembleAndroidTest
-UNSIGNED_TEST_APK=${APP_DIR}/app/build/outputs/apk/basic/release/app-basic-debug-androidTest.apk
+GRADLE_TEST_TASK=app:assembleBasicDebugAndroidTest
+UNSIGNED_TEST_APK=${APP_DIR}/app/build/outputs/apk/androidTest/basic/debug/app-basic-debug-androidTest.apk
 SIGNED_TEST_APK=${PROJECT_DIR}/eduVPNTest-${GIT_TAG}.apk
 
 # Let's Connect!
@@ -49,7 +49,7 @@ SIGNED_TEST_APK=${PROJECT_DIR}/eduVPNTest-${GIT_TAG}.apk
     export ANDROID_HOME=${SDK_DIR}
     cd "${APP_DIR}" || exit
     ./gradlew ${GRADLE_TASK} --warning-mode all --stacktrace || exit
-    if [ "app:assembleBasicRelease" = "${GRADLE_TASK}" ]; then
+    if [ "app:assembleBasicDebug" = "${GRADLE_TASK}" ]; then
         ./gradlew ${GRADLE_TEST_TASK} --warning-mode all --stacktrace || exit 
     fi
 )
@@ -63,9 +63,8 @@ SIGNED_TEST_APK=${PROJECT_DIR}/eduVPNTest-${GIT_TAG}.apk
     BUILD_TOOLS_VERSION=$(ls ${SDK_DIR}/build-tools/ | sort -r | head -1)
     ${SDK_DIR}/build-tools/${BUILD_TOOLS_VERSION}/apksigner sign --ks "${KEY_STORE}" "${UNSIGNED_APK}" || exit
     cp "${UNSIGNED_APK}" "${SIGNED_APK}" || exit
-    if [ "app:assembleBasicRelease" = "${GRADLE_TASK}" ]; then
+    if [ "app:assembleBasicDebug" = "${GRADLE_TASK}" ]; then
         ${SDK_DIR}/build-tools/${BUILD_TOOLS_VERSION}/apksigner sign --ks "${KEY_STORE}" "${UNSIGNED_TEST_APK}" || exit
         cp "${UNSIGNED_TEST_APK}" "${SIGNED_TEST_APK}" || exit
     fi
 )
-
